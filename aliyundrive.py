a87e3a292326c65f27555ff9fb185cf8446d4024
libuke <libuke025@gmail.com>
2023-06-12 01:59:14 +0800

    add request retry.

--- aliyundrive.py
+++ aliyundrive.py
@@ -1,7 +1,7 @@
 import json
 import requests
-
 from aliyundrive_info import AliyundriveInfo
+from tenacity import retry, stop_after_attempt, wait_fixed, RetryError
 
 class Aliyundrive:
     """
@@ -12,33 +12,39 @@ class Aliyundrive:
     """
     def aliyundrive_check_in(self, token: str) -> AliyundriveInfo:
         info = AliyundriveInfo(
-                success=False, 
-                user_name='', 
-                signin_count=-1, 
-                message='', 
-                reward_notice='')
+            success=False, 
+            user_name='', 
+            signin_count=-1, 
+            message='', 
+            reward_notice=''
+        )
 
-        flag, user_name, access_token, message = self._get_access_token(token)
-        
-        if not flag:
-            info.message = f'get_access_token error: {message}'
+        def handle_error(error_message: str) -> AliyundriveInfo:
+            info.message = error_message
             return info
         
-        flag, signin_count, message = self._check_in(access_token)
-        if not flag:
-            info.message = f'check_in error: {message}'
+        try:
+            flag, user_name, access_token, message = self._get_access_token(token)
+            if not flag:
+                return handle_error(info, f'get_access_token error: {message}')
+            
+            flag, signin_count, message = self._check_in(access_token)
+            if not flag:
+                return handle_error(info, f'check_in error: {message}')
+            
+            flag, message = self._get_reward(access_token, signin_count)
+            if not flag:
+                return handle_error(info, f'get_reward error: {message}')
+            
+            info.success = True
+            info.user_name = user_name
+            info.signin_count = signin_count
+            info.reward_notice = message
+
             return info
-        
-        flag, message = self._get_reward(access_token, signin_count)
-        if not flag:
-            info.message = f'get_reward error: {message}'
-            return info
-        
-        info.success = True
-        info.user_name = user_name
-        info.signin_count = signin_count
-        info.reward_notice = message
-        return info
+
+        except RetryError as e:
+            return handle_error(f'Unexpected error occurred: {str(e)}')
         
     """
     获取access_token
@@ -49,19 +55,18 @@ class Aliyundrive:
     :return tuple[2]: access_token
     :return tuple[3]: message
     """
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
     def _get_access_token(self, token: str) -> tuple[bool, str, str, str]:
-        data = requests.post(
-            url='https://auth.aliyundrive.com/v2/account/token', 
-            json={ 'grant_type': 'refresh_token', 'refresh_token': token }
-        ).json()
+        url = 'https://auth.aliyundrive.com/v2/account/token'
+        payload = {'grant_type': 'refresh_token', 'refresh_token': token}
 
-        if ('code' in data and data['code'] in [
-            'RefreshTokenExpired', 
-            'InvalidParameter.RefreshToken'
-        ]):
+        response = requests.post(url, json=payload, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+
+        if 'code' in data and data['code'] in ['RefreshTokenExpired', 'InvalidParameter.RefreshToken']:
             return False, '', '', data['message']
-        
-        
+
         nick_name, user_name = data['nick_name'], data['user_name']
         name = nick_name if nick_name else user_name
         access_token = data['access_token']
@@ -75,17 +80,20 @@ class Aliyundrive:
     :return tuple[1]: 签到次数
     :return tuple[2]: message
     """
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
     def _check_in(self, access_token: str) -> tuple[bool, int, str]:
-        data = requests.post(
-            url='https://member.aliyundrive.com/v1/activity/sign_in_list',
-            json={ 'isReward': False },
-            params={ '_rx-s': 'mobile' },
-            headers={ 'Authorization': f'Bearer {access_token}' }
-        ).json()
-  
+        url = 'https://member.aliyundrive.com/v1/activity/sign_in_list'
+        payload = {'isReward': False}
+        params = {'_rx-s': 'mobile'}
+        headers = {'Authorization': f'Bearer {access_token}'}
+
+        response = requests.post(url, json=payload, params=params, headers=headers, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+
         if 'success' not in data:
             return False, -1, data['message']
-        
+
         success = data['success']
         signin_count = data['result']['signInCount']
 
@@ -99,13 +107,16 @@ class Aliyundrive:
     :return tuple[0]: 是否成功
     :return tuple[1]: message 奖励信息或者出错信息
     """
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
     def _get_reward(self, access_token: str, sign_day: int) -> tuple[bool, str]:
-        data = requests.post(
-            url='https://member.aliyundrive.com/v1/activity/sign_in_reward',
-            json={ 'signInDay': sign_day },
-            params={ '_rx-s': 'mobile' },
-            headers={ 'Authorization': f'Bearer {access_token}' }
-        ).json()
+        url = 'https://member.aliyundrive.com/v1/activity/sign_in_reward'
+        payload = {'signInDay': sign_day}
+        params = {'_rx-s': 'mobile'}
+        headers = {'Authorization': f'Bearer {access_token}'}
+
+        response = requests.post(url, json=payload, params=params, headers=headers, timeout=5)
+        response.raise_for_status()
+        data = response.json()
 
         if 'result' not in data:
             return False, data['message']
--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import os
 import re
 import argparse
-
-from message_send import MessageSend
 from aliyundrive import Aliyundrive
+from message_send import MessageSend
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--token_string', type=str, required=True)
+    args = parser.parse_args()
 
-    args= parser.parse_args()
     token_string = args.token_string
-    pushplus_token = os.environ.get('PUSHPLUS_TOKEN', None)
-    serverChan_sendkey = os.environ.get('SERVERCHAN_SENDKEY', None)
-    weCom_webhook = os.environ.get('WECOM_WEBHOOK', None)
-    bark_deviceKey = os.environ.get('BARK_DEVICEKEY', None)
+    pushplus_token = os.environ.get('PUSHPLUS_TOKEN')
+    serverChan_sendkey = os.environ.get('SERVERCHAN_SENDKEY')
+    weCom_webhook = os.environ.get('WECOM_WEBHOOK')
+    bark_deviceKey = os.environ.get('BARK_DEVICEKEY')
 
     message_tokens = {
         'pushplus_token': pushplus_token,
@@ -25,20 +24,23 @@ if __name__ == '__main__':
 
     token_string = token_string.split(',')
     ali = Aliyundrive()
-    message_all = ''
+    message_all = []
+
     for idx, token in enumerate(token_string):
         result = ali.aliyundrive_check_in(token)
-        message_all = f'{message_all}{result}'
+        message_all.append(str(result))
 
-        if idx < len(token_string) - 1:  
-            message_all = f'{message_all}\n--\n'
+        if idx < len(token_string) - 1:
+            message_all.append('--')
 
     title = '阿里云盘签到结果'
-    message_all = f'{title}\n{message_all}'
-    message_all = re.sub('\n+','\n', message_all)
-    if message_all.endswith('\n'): message_all = message_all[:-1]
+    message_all = '\n'.join(message_all)
+    message_all = re.sub('\n+', '\n', message_all).rstrip('\n')
 
     message_send = MessageSend()
     message_send.send_all(message_tokens, title, message_all)
 
     print('finish')
+
+if __name__ == '__main__':
+    main()
