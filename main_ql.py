a19131c1713e5d846e1943a4801d8f8342af228e
lelemka0 <20322043+lelemka0@users.noreply.github.com>
2023-11-12 05:04:57 +0800

    feat: optimize for Qinglong

---
+++ main_ql.py
@@ -0,0 +1,29 @@
+import os
+from aliyundrive import Aliyundrive
+
+
+
+def main():
+    token_string = os.environ.get('refreshToken')
+    token_string = token_string.split('&')
+
+    ali = Aliyundrive()
+    message_all = []
+
+    for idx, token in enumerate(token_string):
+        result = ali.aliyundrive_check_in(token)
+        message_all.append(str(result))
+
+        if idx < len(token_string) - 1:
+            message_all.append('--')
+
+    #TODO: 发送通知
+    #title = '阿里云盘签到结果'
+    #message_all = '\n'.join(message_all)
+    #message_all = re.sub('\n+', '\n', message_all).rstrip('\n')
+
+    print('finish')
+
+
+if __name__ == '__main__':
+    main()
